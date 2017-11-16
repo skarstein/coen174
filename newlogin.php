04b096c923d6c7a9aaa542e0e43a269f219a1a47
angelina r poole <apoole@linux60412.dc.engr.scu.edu>
2017-11-15 21:47:26 -0800

    bug fix

--- newlogin.php
+++ newlogin.php
@@ -44,7 +44,7 @@
 
                 if($hashed_password==$row["password"])
                 {
-                   $_SESSION['users'] = $username;
+                   $_SESSION['user'] = $username;
                    $_SESSION['type'] = $row["type"];
 
                    //if the user registered as a student
