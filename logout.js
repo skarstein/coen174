8e64c968c518fca1ba42d96cfa8c7d1a96004f41
Colby Harper <charper@scu.edu>
2017-11-25 22:56:24 -0500

    have logout.js call logout.php

--- logout.js
+++ logout.js
@@ -1,3 +1,11 @@
 function logout() {
-  window.location.href = 'homepage.html';
+  var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+              location.reload(true);
+      }
+    };
+    xhttp.open("GET", "logout.php");
+    xhttp.send();
+    window.location.href = "homepage.html";
 }
