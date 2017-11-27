33bcf7eb353fdf2864c989c8dac6f9db21d923de
angelina r poole <apoole@linux60813.dc.engr.scu.edu>
2017-11-27 13:47:20 -0800

    populate edit form with existing values, then update the row when submitted.

--- editPage.php
+++ editPage.php
@@ -1,41 +1,62 @@
 <?php
-    // A simple PHP script demonstrating how to connect to MySQL.
-    // Press the 'Run' button on the top to start the web server,
-    // then click the URL that is emitted to the Output tab of the console.
-
+    session_start();
     $servername = "dbserver.engr.scu.edu";
-    $username = "shu";
-    $password = "group2";
-    $database = "sdb_shu";
+    $username = 'shu';
+    $password = 'group2';
+    $dbname = 'sdb_shu';
 
-    // Create connection
-    $db = mysqli_connect($servername, $username, $password, $database);
+    $connection = mysqli_connect($servername, $username, $password, $dbname);
+    $sql = "SELECT * FROM books WHERE title ='".$_GET['title']."';";
+    $result = $connection->query($sql);
+    $row = $result->fetch_assoc();
+    $_SESSION['bookid'] = $row['id'];
+?>
 
-    // Check connection
-    if ($db->connect_error) {
-        die("Connection failed: " . $db->connect_error);
-    }
+<!DOCTYPE html>
+<HTML>
+<head>
+  <link rel="stylesheet" type="text/css" href="bookinput.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+</head>
 
-    $booktitle = $db->real_escape_string($_POST["title"]);
-    $authorname = $db->real_escape_string($_POST["author"]);
-    $copyrightdate = $db->real_escape_string($_POST["copyright"]);
-    $lexilelevel = $db->real_escape_string($_POST["lexile"]);
-    $numberofpages = $db->real_escape_string($_POST["pages"]);
-    $boolrecommended = $db->real_escape_string($_POST["recommended"]);
-    $booktopic = $db->real_escape_string($_POST["topic"]);
-    $bookpprotag_n = $db->real_escape_string($_POST["pprotag_n"]);
-    $booksprotag_n = $db->real_escape_string($_POST["sprotag_n"]);
+<body>
+<title>Edit Book</title>
+<h1>Book Matching Recommendation System</h1>
+<h2>Edit Book Details</h2>
 
-    $sql = "INSERT INTO books(title, author, copyright, lexile, pages, recommended, topic, pprotag_n, sprotag_n) VALUES ('" .$booktitle. "', '" .$authorname. "','" .$copyrightdate."','".$lexilelevel."','".$numberofpages."','".$boolrecommended."','".$booktopic."','".$bookpprotag_n."','".$booksprotag_n."')";
-    //echo($sql);
+  <div class="form_center">
+  <div class="form_mate">
+  <form action = "editbook.php" method = "post">
+  <fieldset>
+      <b>Book Title</b><br>
+      <input type="text" placeholder="Book Title" id = "title" name="title" value="<?php echo $row['title'];?>">
 
-    if ($db->query($sql) === TRUE) {
-        echo '<script>alert("You have successfully added a book");</script>';
-        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=accountpage.php">';
-    } else {
-          echo '<script>alert("Error: Unable to edit book");</script>';
-	  echo '<META HTTP-EQUIV="Refresh" Content="0; URL=accountpage.php">';
-    }
+      <br><b>Author</b><br>
+      <input type="text" placeholder="Author's Name" id = "author" name="author" value="<?php echo $row['author'];?>">
 
-    $db->close();
-    ?>
+      <br><b>Copyright Date</b><br>
+      <input type="number" placeholder="Copyright Date" id = "copyright" name="copyright" value="<?php echo $row['copyright'];?>">
+      <br><b>Lexile Reading Level</b></br>
+      <input type="number" placeholder="Lexile Rate" id = "lexile" name="lexile" required value="<?php echo $row['lexile'];?>">
+      <br><b>Number of Pages</b></br>
+      <input type="number" placeholder="Number of Pages" id = "pages" name="pages" required value="<?php echo $row['pages'];?>">
+      <br><label><b>Recommended</b><br>
+      <select name = "recommended">
+      <option value = "0"> Yes </option>
+      <option value = "1"> No </option>
+      </select> <br />
+      <br><b>Topic</b></br>
+      <input type="text" placeholder="Topic" id = "topic" name="topic" required value="<?php echo $row['topic'];?>">
+      <br><b>Primary Protagonist Nature</b></br>
+      <input type="text" placeholder="Primary Protagonist Nature" id = "pprotag_n" name="pprotag_n" required value="<?php echo $row['pprotag_n'];?>">
+      <br><b>Secondary Protagonist Nature</b></br>
+      <input type="text" placeholder="Secondary Protagonist Nature" id = "sprotag_n" name="sprotag_n" required value="<?php echo $row['sprotag_n'];?>">
+        <button type="submit">Edit Book</button>
+  </fieldset>
+  </form>
+  </div>
+  </div>
+
+  </body>
+
+</HTML>
