fa1b7acb55141bcbf59a6fe8f6b871de1cc5e50d
Colby Harper <charper@scu.edu>
2017-11-27 17:17:59 -0800

    change order of code

--- teachersetup.php
+++ teachersetup.php
@@ -1,32 +1,3 @@
-<?php
-
-  session_start();
-  $servername = "dbserver.engr.scu.edu";
-  $usrname = "shu";
-  $pssword = "group2";
-  $database = "sdb_shu";
-
-  // Create connection
-  $db = mysqli_connect($servername, $usrname, $pssword, $database);
-
-  // Check connection
-  if ($db->connect_error) {
-      die("Connection failed: " . $db->connect_error);
-  }
-
-  $course_id = $db->real_escape_string($_POST["course_id"]);
-
-  $sql = "INSERT INTO courses(course_id, user_id) VALUES ('".$course_id."', '".$_SESSION['user_id']."')";
-  if ($db->query($sql) === TRUE) {
-        echo '<script>alert("You have successfully created your first course");</script>';
-        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=accountpage.php">';
-  } else {
-          echo '<script>alert("Error: Unable to create course");</script>';
-          echo '<META HTTP-EQUIV="Refresh" Content="0; URL=teachersetup.php">';
-  }
-  $db->close();
-?>
-
 <!DOCTYPE html>
 <HTML>
 <head>
@@ -55,3 +26,32 @@
   </div>
 </body>
 </HTML>
+
+<?php
+
+  session_start();
+  $servername = "dbserver.engr.scu.edu";
+  $usrname = "shu";
+  $pssword = "group2";
+  $database = "sdb_shu";
+
+  // Create connection
+  $db = mysqli_connect($servername, $usrname, $pssword, $database);
+
+  // Check connection
+  if ($db->connect_error) {
+      die("Connection failed: " . $db->connect_error);
+  }
+
+  $course_id = $db->real_escape_string($_POST["course_id"]);
+
+  $sql = "INSERT INTO courses(course_id, user_id) VALUES ('".$course_id."', '".$_SESSION['user_id']."')";
+  if ($db->query($sql) === TRUE) {
+        echo '<script>alert("You have successfully created your first course");</script>';
+        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=accountpage.php">';
+  } else {
+          echo '<script>alert("Error: Unable to create course");</script>';
+          echo '<META HTTP-EQUIV="Refresh" Content="0; URL=teachersetup.php">';
+  }
+  $db->close();
+?>
