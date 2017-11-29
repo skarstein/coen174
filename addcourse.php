24ca3720e4dd62468c6a85f049d1ce87583b7b74
Colby Harper <charper@scu.edu>
2017-11-29 14:58:46 -0800

    only unique course id's allowed

--- addcourse.php
+++ addcourse.php
@@ -17,13 +17,21 @@
   $course_id = $db->real_escape_string($_POST["course_id"]);
   $name = $db->real_escape_string($_POST["name"]);
 
-  $sql = "INSERT INTO courses(course_id, user_id, name) VALUES ('".$course_id."', '".$_SESSION['user_id']."', '".$name."')";
-  if ($db->query($sql) === TRUE) {
-        echo '<script>alert("You have successfully created a course");</script>';
+  $sql = "SELECT * FROM courses WHERE course_id ="."'".$course_id."';";
+  $result = $db->query($sql);
+  if($result->num_rows == 1) {
+        echo '<script>alert("Error: Course ID already taken");</script>';
         echo '<META HTTP-EQUIV="Refresh" Content="0; URL=teacheraccountpage.php">';
-  } else {
-          echo '<script>alert("Error: Unable to create course");</script>';
+  } else
+  {
+    $sql = "INSERT INTO courses(course_id, user_id, name) VALUES ('".$course_id."', '".$_SESSION['user_id']."', '".$name."')";
+    if ($db->query($sql) === TRUE) {
+          echo '<script>alert("You have successfully created a course");</script>';
           echo '<META HTTP-EQUIV="Refresh" Content="0; URL=teacheraccountpage.php">';
+    } else {
+            echo '<script>alert("Error: Unable to create course");</script>';
+            echo '<META HTTP-EQUIV="Refresh" Content="0; URL=teacheraccountpage.php">';
+    }
   }
   $db->close();
 ?>
