0d60bbdcd205ad560997b9d5fa4d629f0974dafa
Colby Harper <charper@scu.edu>
2017-11-28 23:31:30 -0800

    convert list to table

--- bookresults.php
+++ bookresults.php
@@ -23,20 +23,42 @@
     <a href="studentaccountpage.php"><button type="button" class="back-btn">Back</button></a>
 <hr>
     <h4>Recommended Books</h4>
-    <table border=0 class="rectangle-list">
-        <tr>
-            <td>
-            <ol>
-            <?php
-                while($row=mysqli_fetch_array($result)) {
-                    echo '<li><p>'.htmlspecialchars($row['title']).'</p></li>';
-                }
 
-                $connection->close();
-            ?>
-            </ol>
-            </td>
-        </tr>
+    <table class = "table-rwd" id="books">
+     <thead>
+       <tr>
+         <th style="color:#dd5;">Title</th>
+         <th style="color:#dd5;">Author</th>
+         <th style="color:#dd5;">Copyright</th>
+         <th style="color:#dd5;">Lexile</th>
+         <th style="color:#dd5;">Pages</th>
+         <th style="color:#dd5;">Recommended</th>
+         <th style="color:#dd5;">Topic</th>
+         <th style="color:#dd5;">Protagonist Nature</th>
+         <th style="color:#dd5;">Secondary Protagonist Nature</th>
+         <th style="color:#dd5;">Rank</th>
+       </tr>
+     </thead>
+     <tbody>
+     <?php
+     while($row=$result->fetch_assoc()){
+       echo
+         "<tr>
+           <td align=\"center\">".htmlspecialchars($row['title'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['author'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['copyright'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['lexile'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['pages'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['recommended'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['topic'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['pprotag_n'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['sprotag_n'])."</td>
+           <td align=\"center\">".htmlspecialchars($row['rank'])."</td>
+          </tr>";
+      }
+      $connection->close();
+      ?>
+    </tbody>
     </table>
 </body>
 </html>
